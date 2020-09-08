c4d8b1514696f2eb24dc82424e2ecd7888bbe43b
leveses <boda.zsombor10@gmail.com>
2020-09-08 11:16:30 +0200

    gep valasztasa

--- ismetles/Program.cs
+++ ismetles/Program.cs
@@ -6,7 +6,14 @@ namespace ismetles
     {
         static void Main(string[] args)
         {
+            Random vel = new Random();
+            string[] lehetoseg = new string[]{ "Kő", "Papír", "Olló"};
+            int gepValasz = vel.Next(0,3);
+
+            Console.WriteLine("Gép választása: {0}",lehetoseg[gepValasz]);
+            string jatekosV = Console.ReadLine();
             
+            Console.ReadKey();
         }
     }
 }
