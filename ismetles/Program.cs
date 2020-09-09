65928c28214d7db099ccd08900c2a6be25df6f35
leveses <boda.zsombor10@gmail.com>
2020-09-09 14:55:37 +0200

    gepvalasztas és jatekosvalasztas metódus

--- ismetles/Program.cs
+++ ismetles/Program.cs
@@ -5,6 +5,16 @@ namespace ismetles
     class Program
     {
         static string[] lehetoseg = new string[] { "Kő", "Papír", "Olló" };
+        static int GepValasztas()
+        {
+            Random vel = new Random();
+            return vel.Next(0, 3);
+        }
+        static int JatekosValaszt()
+        {
+            Console.WriteLine("Kő: 0, Papír: 1, Olló: 2");
+            return int.Parse(Console.ReadLine());
+        }
         static void EredmenyKiiras(int gep, int ember)
         {
             Console.WriteLine("Gép: {0} --- Játékos {1}", lehetoseg[gep], lehetoseg[ember]);
@@ -38,13 +48,12 @@ namespace ismetles
         }
         static void Main(string[] args)
         {
-            Random vel = new Random();
-            
-            int gep = vel.Next(0,3);
 
+
+            int gep = GepValasztas();
+            int ember = JatekosValaszt();
             
-            Console.WriteLine("Kő: 0, Papír: 1, Olló: 2");
-            int ember =  int.Parse(Console.ReadLine());
+            
             Console.WriteLine("Játékos válastztása: {0}",lehetoseg[ember]);
             Console.WriteLine("Gép választása: {0}",lehetoseg[gep]);
             
